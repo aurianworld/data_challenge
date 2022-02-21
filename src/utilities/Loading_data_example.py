189914e2400e6dab8af1cdc0009924b75aa81449
Georges Le Bellier <glebellier@protonmail.com>
2022-02-21 11:46:45 +0100

    Fix h5f files component names

--- src/utilities/Loading_data_example.py
+++ src/utilities/Loading_data_example.py
@@ -7,16 +7,16 @@ import cv2
 # Loading data tables from H5py files
 #######################################################################
 
-h5f = h5py.File('dataset/data_train_images.h5','r')
-train_images = h5f['dataset/data_train_images'][:]
+h5f = h5py.File('./dataset/data_train_images.h5', 'r')
+train_images = h5f['data_train_images'][:]
 h5f.close()
 
-h5f = h5py.File('dataset/data_train_landmarks.h5','r')
-train_landmarks = h5f['dataset/data_train_landmarks'][:]
+h5f = h5py.File('./dataset/data_train_landmarks.h5', 'r')
+train_landmarks = h5f['data_train_landmarks'][:]
 h5f.close()
 
-h5f = h5py.File('dataset/data_train_labels.h5','r')
-train_labels = h5f['dataset/data_train_labels'][:]
+h5f = h5py.File('./dataset/data_train_labels.h5', 'r')
+train_labels = h5f['data_train_labels'][:]
 h5f.close()
 
 #######################################################################
@@ -35,22 +35,24 @@ print("\nLabels")
 print(train_labels.shape)
 
 #######################################################################
-# display an example by giving the sequence number ( Train in [0,540[ ) 
+# display an example by giving the sequence number ( Train in [0,540[ )
 # and the sequence frame number ( in [0,9] )
 #######################################################################
 
 sequence = 127
 frame = 7
 
-img = train_images[sequence,frame]
-landmarks = train_landmarks[sequence,frame]
+img = train_images[sequence, frame]
+landmarks = train_landmarks[sequence, frame]
 
-expressions = ['happiness','fear','surprise','anger','disgust','sadness']
+expressions = ['happiness', 'fear', 'surprise', 'anger', 'disgust', 'sadness']
 
-print("Label (sequence : " + str(sequence) + ") = " + str(train_labels[sequence]) + " -> " + expressions[int(train_labels[sequence])])
+print("Label (sequence : " + str(sequence) + ") = " +
+      str(train_labels[sequence]) + " -> " +
+      expressions[int(train_labels[sequence])])
 
 for p in landmarks:
-    img = cv2.circle(img, (p[0],p[1]), 2, (255,255,255), 2)
+    img = cv2.circle(img, (p[0], p[1]), 2, (255, 255, 255), 2)
 
 plt.imshow(img)
 plt.show()
